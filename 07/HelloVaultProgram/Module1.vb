8c5845a86f7e0b75bb3d33bcd02e3ff9e8a32e10
jliliamen <amen@bluebyte.biz>
2021-01-01 20:06:39 -0800

    Added 01, 02, 03, 04, 05, 06, 07, 08 and 09

---
+++ 07/HelloVaultProgram/Module1.vb
@@ -0,0 +1,14 @@
+﻿Imports EPDM.Interop.epdm
+Module Module1
+    Sub Main()
+
+        Dim vault As New EdmVault5Class()
+        Dim appMainWindowHandle = Process.GetCurrentProcess().MainWindowHandle.ToInt32
+
+        'print hello vault message
+        vault.MsgBox(appMainWindowHandle, "Hello Vault!")
+
+
+    End Sub
+
+End Module
