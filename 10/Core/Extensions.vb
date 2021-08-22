80f381b485a28f42f63a1084a37fd88d991d07d0
Amen Jlili (Home) <amen@bluebyte.biz>
2021-08-22 12:14:25 -0700

    Added lesson 10

---
+++ 10/Core/Extensions.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Runtime.CompilerServices
+Imports EPDM.Interop.epdm
+
+Public Module Extensions
+
+    ''' <summary>
+    ''' Returns an array containing vault names.
+    ''' </summary>
+    ''' <param name="vault">Vault object.</param>
+    ''' <param name="OnlyLoggedIn">Only return logged in vault.</param>
+    ''' <returns>An array of strings.</returns>
+    <Extension()>
+    Public Function GetVaultNames(ByVal vault As EdmVault5Class, ByVal OnlyLoggedIn As Boolean) As String()
+
+        If vault Is Nothing Then
+            Throw New ArgumentNullException("vault")
+        End If
+
+        Dim viewInfos As EdmViewInfo() = Nothing
+
+        vault.GetVaultViews(viewInfos, OnlyLoggedIn)
+
+        Dim vaultNames As New List(Of String)
+
+        If viewInfos IsNot Nothing Then
+
+            For Each viewInfo As EdmViewInfo In viewInfos
+                vaultNames.Add(viewInfo.mbsVaultName)
+            Next
+
+        End If
+
+        Return vaultNames.ToArray()
+
+    End Function
+
+
+
+
+End Module
---
+++ 10/HelloVaultProgram/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports Core
+Imports EPDM.Interop.epdm
+Module Module1
+    Sub Main()
+
+        Dim vault As New EdmVault5Class()
+        Dim vaultNames = vault.GetVaultNames(False)
+        If vaultNames IsNot Nothing Then
+            For Each vaultName As String In vaultNames
+                Console.WriteLine(vaultName)
+            Next
+        End If
+    End Sub
+
+End Module
